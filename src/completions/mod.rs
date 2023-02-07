10e402caf650a0e456a13ea711356fc2f4c95f59
0xKitsune <0xKitsune@protonmail.com>
2023-02-07 01:54:47 -0500

    updated completionrequest to builder structure

--- src/completions/mod.rs
+++ src/completions/mod.rs
@@ -29,53 +29,67 @@ impl CompletionRequest {
         }
     }
 
-    pub fn suffix(&mut self, suffix: String) {
+    pub fn suffix(mut self, suffix: String) -> Self {
         self.suffix = Some(suffix);
+        self
     }
 
-    pub fn max_tokens(&mut self, max_tokens: i32) {
+    pub fn max_tokens(mut self, max_tokens: i32) -> Self {
         self.max_tokens = Some(max_tokens);
+        self
     }
-    pub fn temperature(&mut self, temperature: f64) {
+    pub fn temperature(mut self, temperature: f64) -> Self {
         self.temperature = Some(temperature);
+        self
     }
-    pub fn top_p(&mut self, top_p: f64) {
+    pub fn top_p(mut self, top_p: f64) -> Self {
         self.top_p = Some(top_p);
+        self
     }
-    pub fn n(&mut self, n: i32) {
+    pub fn n(mut self, n: i32) -> Self {
         self.n = Some(n);
+        self
     }
-    pub fn stream(&mut self, stream: bool) {
+    pub fn stream(mut self, stream: bool) -> Self {
         self.stream = Some(stream);
+        self
     }
 
-    pub fn lob_probs(&mut self, lob_probs: i32) {
+    pub fn lob_probs(mut self, lob_probs: i32) -> Self {
         self.lob_probs = Some(lob_probs);
+        self
     }
 
-    pub fn echo(&mut self, echo: bool) {
+    pub fn echo(mut self, echo: bool) -> Self {
         self.echo = Some(echo);
+        self
     }
-    pub fn stop(&mut self, stop: bool) {
+    pub fn stop(mut self, stop: bool) -> Self {
         self.stop = Some(stop);
+        self
     }
 
-    pub fn presence_penalty(&mut self, presence_penalty: f64) {
+    pub fn presence_penalty(mut self, presence_penalty: f64) -> Self {
         self.presence_penalty = Some(presence_penalty);
+        self
     }
-    pub fn frequency_penalty(&mut self, frequency_penalty: f64) {
+    pub fn frequency_penalty(mut self, frequency_penalty: f64) -> Self {
         self.frequency_penalty = Some(frequency_penalty);
+        self
     }
 
-    pub fn best_of(&mut self, best_of: i32) {
+    pub fn best_of(mut self, best_of: i32) -> Self {
         self.best_of = Some(best_of);
+        self
     }
-    pub fn logit_bias(&mut self, logit_bias: String) {
+    pub fn logit_bias(mut self, logit_bias: String) -> Self {
         self.logit_bias = Some(logit_bias);
+        self
     }
 
-    pub fn user(&mut self, user: String) {
+    pub fn user(mut self, user: String) -> Self {
         self.user = Some(user);
+        self
     }
 }
 
