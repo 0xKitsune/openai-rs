00dd228c647ba38bba035a05155500cd57b585a4
0xKitsune <0xKitsune@protonmail.com>
2023-02-08 20:38:26 -0500

    formatting example

--- examples/completion.rs
+++ examples/completion.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use openai::{
     client,
     completions::{CompletionRequest, CompletionResponse},
-    models::{self, Model},
+    models::{self},
 };
 
 #[tokio::main]
@@ -14,15 +14,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let prompt = "What is a good name for a Rust library that wraps the openai api?";
     println!("{:?}", prompt);
+
     let model = models::TEXT_DAVINCI_003;
     let response = open_ai_client.complete(&model, prompt).await?;
+
     println!("{} \n\n", response.choices[0].text);
+
     let prompt = "Can you write me a poem about the Rust programming language?";
     println!("{:?}", prompt);
+
     let request = CompletionRequest::new("text-davinci-003", prompt);
+
     let response = open_ai_client
         .send_request::<CompletionRequest, CompletionResponse>(request)
         .await?;
+
     println!("{}", response.choices[0].text);
 
     Ok(())
