fe52c9b33d7d47fa66babc2e3a92b7f06461d0cb
0xKitsune <0xKitsune@protonmail.com>
2023-07-09 14:50:49 -0400

    updated the completion example

--- examples/completion.rs
+++ examples/completion.rs
@@ -11,15 +11,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let api_key = std::env::var("OPENAI_API_KEY").expect("Could not get api token");
     let open_ai_client = client::OpenAIClient::new(api_key);
 
+    // Create a new model
+    let model = models::GPT_4;
+
     // Create a new prompt
     let messages = vec![Message::new(
         "Can you write me a simple Rust server that can handle a 'Ping' request, returning a 'Pong' message?",
     )];
     println!("{}", messages[0].content);
-    // Create a new model
-    let model = models::GPT_4;
 
-    //use the client.complete() method to send a prompt to the completion endpoint
+    // Send the chat completion request to OpenAI
     let response = open_ai_client.chat_completion(&model, messages).await?;
     println!("{} \n\n", response.choices[0].message.content);
 
